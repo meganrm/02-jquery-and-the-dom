24b21703a625da925b7e4c1e7c744946866754fc
meganrm <meganrm@gmail.com>
2016-08-16 13:33:24 -0700

    added jquery to read local data and append to blog in reverse chronological order

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,32 +1,41 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the js object passed in to complete this constructor function:
+  // TODO: Done Use the js object passed in to complete this constructor function:
   // Save ALL the properties of `opts` into `this`
-  this.author = opts.author;
+  this.title=opts.title;
+  this.category=opts.category;
+  this.author=opts.author;
+  this.authorUrl=opts.authorUrl;
+  this.publishedOn=opts.publishedOn;
+  this.body=opts.body;
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('.byline a').text(this.author).attr('href', this.authorUrl);
+  $newArticle.find('.article-body').append(this.body);
 
-  /* TODO: Now use jQuery to fill in the rest of the current
+  /* TODO: Done Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
-    1. author name,
-    2. author url,
-    3. article title,
-    4. article body, and
+    1. author name, done
+    2. author url, done
+    3. article title, done
+
+    4. article body, done
     5. publication date. */
     // Display the date as a relative number of 'days ago'
-    $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
-    /* TODO: This cloned article is no longer a template,
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.removeClass('template');
+    /* TODO: Done This cloned article is no longer a template,
     as it now has real data attached to it! We need to account
     for that before this current article gets rendered to our
     DOM. */
-    return $newArticle;
+  return $newArticle;
 };
 
 ourLocalData.sort(function(firstElement, secondElement) {
